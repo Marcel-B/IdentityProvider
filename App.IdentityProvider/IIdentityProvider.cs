4b0b599b13297d8b20c6489c2e946408cba315f3
Marcel Benders <git@b-velop.com>
2019-01-03 09:06:04 +0100

    update url and add new method to call

--- App.IdentityProvider/IIdentityProvider.cs
+++ App.IdentityProvider/IIdentityProvider.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
 
-namespace com.b_velop.App.IdentityProvider
-{
-    public interface IIdentityProvider
-    {
-        Task<string> GetIdentityToken(string clientId, string secret, string scope);
+namespace com.b_velop.App.IdentityProvider {
+    public interface IIdentityProvider {
+        Task<string> GetIdentityToken (string clientId, string secret, string scope);
+        Task<string> GetIdentityToken (
+            string clientId,
+            string secret,
+            string scope,
+            string authorityUrl);
     }
 }
--- App.IdentityProvider/IdentityProvider.cs
+++ App.IdentityProvider/IdentityProvider.cs
@@ -7,49 +7,49 @@ using System.Threading.Tasks;
 using com.b_velop.App.IdentityProvider.Model;
 using NLog;
 
-namespace com.b_velop.App.IdentityProvider
-{
-    public class IdentityProvider : IIdentityProvider
-    {
-        public IdentityProvider(ILogger logger = null)
-        {
+namespace com.b_velop.App.IdentityProvider {
+    public class IdentityProvider : IIdentityProvider {
+        public IdentityProvider (ILogger logger = null) {
             _logger = logger;
         }
 
         private readonly ILogger _logger;
-        public async Task<string> GetIdentityToken(
+        public async Task<string> GetIdentityToken (
             string clientId,
             string secret,
-            string scope)
-        {
-            const string identityUrl = "https://identity.marcelbenders.de/connect/token";
-            var body = new List<KeyValuePair<string, string>>{
-                new KeyValuePair<string, string>("grant_type", "client_credentials"),
-                new KeyValuePair<string, string>("scope", scope),
-            };
+            string scope,
+            string authorityUrl) {
+            var body = new List<KeyValuePair<string, string>> {
+                    new KeyValuePair<string, string> ("grant_type", "client_credentials"),
+                    new KeyValuePair<string, string> ("scope", scope),
+                };
             Token token = null;
-            using (var content = new FormUrlEncodedContent(body))
-            using (var client = new HttpClient())
-            {
-                var credentials = Convert.ToBase64String(
-                    Encoding.ASCII.GetBytes($"{clientId}:{secret}"));
+            using (var content = new FormUrlEncodedContent (body))
+            using (var client = new HttpClient ()) {
+                var credentials = Convert.ToBase64String (
+                    Encoding.ASCII.GetBytes ($"{clientId}:{secret}"));
 
-                content.Headers.Clear();
-                content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                content.Headers.Clear ();
+                content.Headers.Add ("Content-Type", "application/x-www-form-urlencoded");
                 client.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue("Basic", credentials);
-                var result = await client.PostAsync(identityUrl, content);
-                if (result.IsSuccessStatusCode)
-                {
-                    var str = await result.Content.ReadAsStringAsync();
-                    token = Token.FromJson(str);
-                }
-                else
-                {
-                    _logger?.Log(LogLevel.Error, $"Error occured while calling https://identity.marcelbenders.de. Message: '{result.ReasonPhrase}' Server Status Code: '{result.StatusCode}'");
+                    new AuthenticationHeaderValue ("Basic", credentials);
+                var result = await client.PostAsync (authorityUrl, content);
+                if (result.IsSuccessStatusCode) {
+                    var str = await result.Content.ReadAsStringAsync ();
+                    token = Token.FromJson (str);
+                } else {
+                    _logger?.Log (LogLevel.Error, $"Error occured while calling '{authorityUrl}'. Message: '{result.ReasonPhrase}' Server Status Code: '{result.StatusCode}'");
                 }
             }
             return token != null ? token.AccessToken : string.Empty;
+        }
+
+        public async Task<string> GetIdentityToken (
+            string clientId,
+            string secret,
+            string scope) {
+            const string identityUrl = "https://identity.marcelbenders.de/connect/token";
+            return await GetIdentityToken (clientId, secret, scope, identityUrl);
         }
     }
 }
