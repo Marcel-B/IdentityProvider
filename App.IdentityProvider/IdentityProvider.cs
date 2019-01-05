009e835e756c71c640582b31a2ab9e180366caba
Marcel Benders <git@b-velop.com>
2019-01-05 02:04:15 +0100

    Bugfix url

--- App.IdentityProvider/IdentityProvider.cs
+++ App.IdentityProvider/IdentityProvider.cs
@@ -6,50 +6,63 @@ using System.Text;
 using System.Threading.Tasks;
 using com.b_velop.App.IdentityProvider.Model;
 using NLog;
+using System.IO;
 
-namespace com.b_velop.App.IdentityProvider {
-    public class IdentityProvider : IIdentityProvider {
-        public IdentityProvider (ILogger logger = null) {
+namespace com.b_velop.App.IdentityProvider
+{
+    public class IdentityProvider : IIdentityProvider
+    {
+        public IdentityProvider(
+            ILogger logger = null)
+        {
             _logger = logger;
         }
 
         private readonly ILogger _logger;
-        public async Task<string> GetIdentityToken (
+        public async Task<string> GetIdentityToken(
             string clientId,
             string secret,
             string scope,
-            string authorityUrl) {
+            string authorityUrl)
+        {
+            var authority = Path.Combine(authorityUrl, "/connect/token");
+            _logger.Log(LogLevel.Info, $"Path: {authority}");
             var body = new List<KeyValuePair<string, string>> {
                     new KeyValuePair<string, string> ("grant_type", "client_credentials"),
                     new KeyValuePair<string, string> ("scope", scope),
                 };
             Token token = null;
-            using (var content = new FormUrlEncodedContent (body))
-            using (var client = new HttpClient ()) {
-                var credentials = Convert.ToBase64String (
-                    Encoding.ASCII.GetBytes ($"{clientId}:{secret}"));
+            using (var content = new FormUrlEncodedContent(body))
+            using (var client = new HttpClient())
+            {
+                var credentials = Convert.ToBase64String(
+                    Encoding.ASCII.GetBytes($"{clientId}:{secret}"));
 
-                content.Headers.Clear ();
-                content.Headers.Add ("Content-Type", "application/x-www-form-urlencoded");
+                content.Headers.Clear();
+                content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
                 client.DefaultRequestHeaders.Authorization =
-                    new AuthenticationHeaderValue ("Basic", credentials);
-                var result = await client.PostAsync (authorityUrl, content);
-                if (result.IsSuccessStatusCode) {
-                    var str = await result.Content.ReadAsStringAsync ();
-                    token = Token.FromJson (str);
-                } else {
-                    _logger?.Log (LogLevel.Error, $"Error occured while calling '{authorityUrl}'. Message: '{result.ReasonPhrase}' Server Status Code: '{result.StatusCode}'");
+                    new AuthenticationHeaderValue("Basic", credentials);
+                var result = await client.PostAsync(authority, content);
+                if (result.IsSuccessStatusCode)
+                {
+                    var str = await result.Content.ReadAsStringAsync();
+                    token = Token.FromJson(str);
+                }
+                else
+                {
+                    _logger?.Log(LogLevel.Error, $"Error occured while calling '{authorityUrl}'. Message: '{result.ReasonPhrase}' Server Status Code: '{result.StatusCode}'");
                 }
             }
             return token != null ? token.AccessToken : string.Empty;
         }
 
-        public async Task<string> GetIdentityToken (
+        public async Task<string> GetIdentityToken(
             string clientId,
             string secret,
-            string scope) {
+            string scope)
+        {
             const string identityUrl = "https://identity.marcelbenders.de/connect/token";
-            return await GetIdentityToken (clientId, secret, scope, identityUrl);
+            return await GetIdentityToken(clientId, secret, scope, identityUrl);
         }
     }
 }
